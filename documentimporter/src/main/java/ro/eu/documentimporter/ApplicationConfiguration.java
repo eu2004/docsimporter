4faffd0d98a57a753172a54396517d4424b3d81a
eu2004 <utma.emilian@gmail.com>
2016-08-20 15:06:23 +0200

    added new classes; updated specs.txt

---
+++ documentimporter/src/main/java/ro/eu/documentimporter/ApplicationConfiguration.java
@@ -0,0 +1,44 @@
+package ro.eu.documentimporter;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:app.properties")
+public class ApplicationConfiguration {
+	@Value("${importer.action.in.case.exists}")
+	private String importerActionInCaseExists;
+
+	public ExistingDocumentImporterActions getImporterActionInCaseExists() {
+		return ExistingDocumentImporterActions.findByActionName(importerActionInCaseExists);
+	}
+
+	public static enum ExistingDocumentImporterActions {
+		VERSION("VERSION"), UPDATE("REPLACE"), NEW("CREATE"), IGNORE("SKIP");
+
+		private static Map<String, ExistingDocumentImporterActions> actionsMap = new LinkedHashMap<>(1);
+		private String actionName;
+
+		private ExistingDocumentImporterActions(String actionName) {
+			this.actionName = actionName;
+		}
+
+		public String getActionName() {
+			return actionName;
+		}
+
+		static {
+			for (ExistingDocumentImporterActions value : ExistingDocumentImporterActions.values()) {
+				actionsMap.put(value.actionName, value);
+			}
+		}
+
+		public static ExistingDocumentImporterActions findByActionName(String actionName) {
+			return actionsMap.get(actionName);
+		}
+	}
+}
---
+++ documentimporter/src/main/java/ro/eu/documentimporter/RepositoryDocumentServiceException.java
@@ -0,0 +1,7 @@
+package ro.eu.documentimporter;
+
+public class RepositoryDocumentServiceException extends RuntimeException{
+	public RepositoryDocumentServiceException(String message) {
+		super(message);
+	}
+}
