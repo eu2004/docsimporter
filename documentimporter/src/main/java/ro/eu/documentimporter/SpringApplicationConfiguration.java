f645884415b8e42b129f618f029c208b5060fed2
eu2004 <utma.emilian@gmail.com>
2016-08-20 17:15:46 +0200

    Added spring app config class; removed useless log file.

---
+++ documentimporter/src/main/java/ro/eu/documentimporter/SpringApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package ro.eu.documentimporter;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan
+@PropertySource("classpath:app.properties")
+public class SpringApplicationConfiguration {
+}
