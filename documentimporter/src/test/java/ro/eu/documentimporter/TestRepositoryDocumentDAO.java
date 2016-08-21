76b9e110170d62783b89dde61377366de549c65d
eu2004 <utma.emilian@gmail.com>
2016-08-21 13:37:09 +0200

    Updated

--- documentimporter/src/test/java/ro/eu/documentimporter/TestRepositoryDocumentDAO.java
+++ documentimporter/src/test/java/ro/eu/documentimporter/TestRepositoryDocumentDAO.java
@@ -1,8 +1,9 @@
 package ro.eu.documentimporter;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,85 +19,75 @@ import ro.eu.documentimporter.repository.model.RepositoryEntityIdAttribute;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestApplicationSpringConfiguration.class, loader = AnnotationConfigContextLoader.class)
 public class TestRepositoryDocumentDAO {
-	private static final Logger logger = Logger.getLogger(TestRepositoryDocumentDAO.class);
-
 	@Autowired
 	private RepositoryDocumentDAO mokedRepositoryDocumentDAO;
 
 	@Test
-	public void testDocumentExists() {
-		try {
-			Method method = mokedRepositoryDocumentDAO.getClass().getMethod("getDocumentByCriteria", String.class);
-			Assert.assertTrue(method != null);
-			Assert.assertEquals(Document.class, method.getReturnType());
-		} catch (Exception e) {
-			logger.error("Error checking method existance", e);
-			Assert.fail(e.getMessage());
+	public void testMethodsNames() throws Exception {
+		// given
+		final List<String> expectedMethodsNames = Arrays.asList("getDocumentByCriteria", "getDocumentById",
+				"createDocument", "createDocumentNewVersion", "updateDocument", "replaceDocument");
+		final String expectedInterfaceName = "ro.eu.documentimporter.repository.RepositoryDocumentDAO";
+
+		// when
+		Class foundInterface = null;
+		for (Class currentInterface : mokedRepositoryDocumentDAO.getClass().getInterfaces()) {
+			if (currentInterface.getName().equals(expectedInterfaceName)) {
+				foundInterface = currentInterface;
+				break;
+			}
+		}
+
+		// then
+		Assert.assertTrue(foundInterface != null);
+		Method[] declaredMethods = foundInterface.getDeclaredMethods();
+		Assert.assertEquals(expectedMethodsNames.size(), declaredMethods.length);
+		for (Method declaredMethod : declaredMethods) {
+			Assert.assertTrue(expectedMethodsNames.contains(declaredMethod.getName()));
 		}
 	}
 
 	@Test
-	public void testGetDocumentById() {
-		try {
-			Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("getDocumentById",
-					RepositoryEntityIdAttribute.class);
-			Assert.assertTrue(methodToExecute != null);
-			Assert.assertEquals(Document.class, methodToExecute.getReturnType());
-		} catch (Exception e) {
-			logger.error("Error checking method existance", e);
-			Assert.fail(e.getMessage());
-		}
+	public void testGetDocumentByCriteria() throws Exception {
+		Method method = mokedRepositoryDocumentDAO.getClass().getMethod("getDocumentByCriteria", String.class);
+		Assert.assertTrue(method != null);
+		Assert.assertEquals(Document.class, method.getReturnType());
 	}
-	
+
 	@Test
-	public void testCreateDocument() {
-		try {
-			Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("createDocument",
-					Document.class);
-			Assert.assertTrue(methodToExecute != null);
-			Assert.assertEquals(Document.class, methodToExecute.getReturnType());
-		} catch (Exception e) {
-			logger.error("Error checking method existance", e);
-			Assert.fail(e.getMessage());
-		}
+	public void testGetDocumentById() throws Exception {
+		Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("getDocumentById",
+				RepositoryEntityIdAttribute.class);
+		Assert.assertTrue(methodToExecute != null);
+		Assert.assertEquals(Document.class, methodToExecute.getReturnType());
 	}
-	
+
 	@Test
-	public void testCreateDocumentNewVersion() {
-		try {
-			Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("createDocumentNewVersion",
-					Document.class);
-			Assert.assertTrue(methodToExecute != null);
-			Assert.assertEquals(Document.class, methodToExecute.getReturnType());
-		} catch (Exception e) {
-			logger.error("Error checking method existance", e);
-			Assert.fail(e.getMessage());
-		}
+	public void testCreateDocument() throws Exception {
+		Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("createDocument", Document.class);
+		Assert.assertTrue(methodToExecute != null);
+		Assert.assertEquals(Document.class, methodToExecute.getReturnType());
 	}
-	
+
 	@Test
-	public void testUpdateDocument() {
-		try {
-			Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("updateDocument",
-					Document.class);
-			Assert.assertTrue(methodToExecute != null);
-			Assert.assertEquals(Document.class, methodToExecute.getReturnType());
-		} catch (Exception e) {
-			logger.error("Error checking method existance", e);
-			Assert.fail(e.getMessage());
-		}
+	public void testCreateDocumentNewVersion() throws Exception {
+		Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("createDocumentNewVersion",
+				Document.class);
+		Assert.assertTrue(methodToExecute != null);
+		Assert.assertEquals(Document.class, methodToExecute.getReturnType());
 	}
-	
+
 	@Test
-	public void testReplaceDocument() {
-		try {
-			Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("replaceDocument",
-					Document.class);
-			Assert.assertTrue(methodToExecute != null);
-			Assert.assertEquals(Document.class, methodToExecute.getReturnType());
-		} catch (Exception e) {
-			logger.error("Error checking method existance", e);
-			Assert.fail(e.getMessage());
-		}
+	public void testUpdateDocument() throws Exception {
+		Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("updateDocument", Document.class);
+		Assert.assertTrue(methodToExecute != null);
+		Assert.assertEquals(Document.class, methodToExecute.getReturnType());
+	}
+
+	@Test
+	public void testReplaceDocument() throws Exception {
+		Method methodToExecute = mokedRepositoryDocumentDAO.getClass().getMethod("replaceDocument", Document.class);
+		Assert.assertTrue(methodToExecute != null);
+		Assert.assertEquals(Document.class, methodToExecute.getReturnType());
 	}
 }
