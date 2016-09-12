e52585f2c0bd128c56fe89029d6944d61ccd63a7
eu2004 <utma.emilian@gmail.com>
2016-09-12 22:08:38 +0200

    Updated

--- documentimporter_launcher/src/main/java/ro/eu/documentimporter_launcher/SpringApplicationConfiguration.java
+++ documentimporter_launcher/src/main/java/ro/eu/documentimporter_launcher/SpringApplicationConfiguration.java
@@ -2,10 +2,14 @@ package ro.eu.documentimporter_launcher;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+
+import ro.eu.dctm_documentimporter.DctmApplicationConfiguration;
 
 @Configuration
 @ComponentScan
+@Import({ DctmApplicationConfiguration.class })
 @PropertySource("classpath:app.properties")
 public class SpringApplicationConfiguration {
 }
