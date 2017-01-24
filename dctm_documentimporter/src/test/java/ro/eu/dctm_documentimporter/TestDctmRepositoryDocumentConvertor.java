95a8316a1d02dbd0b636ee10359e626ee422b6be
eu2004 <utma.emilian@gmail.com>
2017-01-24 19:26:06 +0100

    updated

---
+++ dctm_documentimporter/src/test/java/ro/eu/dctm_documentimporter/TestDctmRepositoryDocumentConvertor.java
@@ -0,0 +1,49 @@
+package ro.eu.dctm_documentimporter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import ro.eu.documentimporter.repository.model.RepositoryDocument;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestDctmApplicationConfiguration.class, loader = AnnotationConfigContextLoader.class)
+public class TestDctmRepositoryDocumentConvertor {
+	@Autowired
+	private DctmRepositoryDocumentConvertor dctmRepositoryDocumentConvertor;
+
+	@Test
+	public void testConvert() throws Exception {
+		// given
+		Map<String, String> csvRecord = new HashMap<>();
+		csvRecord.put("r_object_id", "00000000000");
+		csvRecord.put("r_object_type", "dm_document");
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+		Date r_creation_date = new Date();
+		csvRecord.put("r_creation_date", simpleDateFormat.format(r_creation_date));
+
+		// when
+		RepositoryDocument document = dctmRepositoryDocumentConvertor.convert(csvRecord);
+
+		// then
+		Assert.assertNotNull(document);
+		for (Entry<String, String> attribute : csvRecord.entrySet()) {
+			if ("r_creation_date".equals(attribute.getKey())) {
+				Assert.assertEquals(r_creation_date.getTime(),
+						((Date) document.getValue(attribute.getKey())).getTime());
+			} else {
+				Assert.assertEquals(attribute.getValue(), document.getValue(attribute.getKey()).toString());
+			}
+		}
+	}
+}
