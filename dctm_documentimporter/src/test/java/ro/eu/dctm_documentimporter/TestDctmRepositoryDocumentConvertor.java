938b9a65e6c52623723d2cd645afa5a37682cd2a
eu2004 <utma.emilian@gmail.com>
2017-01-24 21:27:54 +0100

    updated

--- dctm_documentimporter/src/test/java/ro/eu/dctm_documentimporter/TestDctmRepositoryDocumentConvertor.java
+++ dctm_documentimporter/src/test/java/ro/eu/dctm_documentimporter/TestDctmRepositoryDocumentConvertor.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import ro.eu.documentimporter.repository.model.RepositoryDocument;
+import ro.eu.documentimporter.repository.model.RepositoryMetadataType;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestDctmApplicationConfiguration.class, loader = AnnotationConfigContextLoader.class)
@@ -27,10 +28,13 @@ public class TestDctmRepositoryDocumentConvertor {
 		// given
 		Map<String, String> csvRecord = new HashMap<>();
 		csvRecord.put("r_object_id", "00000000000");
-		csvRecord.put("r_object_type", "dm_document");
+		csvRecord.put("r_object_type", "dm_document");//string
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 		Date r_creation_date = new Date();
-		csvRecord.put("r_creation_date", simpleDateFormat.format(r_creation_date));
+		csvRecord.put("r_creation_date", simpleDateFormat.format(r_creation_date));//date
+		csvRecord.put("group_permit", "1");//integer
+		csvRecord.put("r_immutable_flag", "true");//boolean
+		csvRecord.put("r_full_content_size", "1000456890");//double
 
 		// when
 		RepositoryDocument document = dctmRepositoryDocumentConvertor.convert(csvRecord);
@@ -38,10 +42,28 @@ public class TestDctmRepositoryDocumentConvertor {
 		// then
 		Assert.assertNotNull(document);
 		for (Entry<String, String> attribute : csvRecord.entrySet()) {
-			if ("r_creation_date".equals(attribute.getKey())) {
+			if ("r_object_type".equals(attribute.getKey())) {
+				Assert.assertEquals(attribute.getValue(), document.getValue(attribute.getKey()).toString());
+				Assert.assertTrue(document.getAttributeValue(attribute.getKey()).getMetadata().getType()
+						.equals(RepositoryMetadataType.STRING));
+			}else if ("r_creation_date".equals(attribute.getKey())) {
 				Assert.assertEquals(simpleDateFormat.format(r_creation_date),
 						simpleDateFormat.format((Date) document.getValue(attribute.getKey())));
-			} else {
+				Assert.assertTrue(document.getAttributeValue(attribute.getKey()).getMetadata().getType()
+						.equals(RepositoryMetadataType.DATE));
+			}else if ("group_permit".equals(attribute.getKey())) {
+				Assert.assertEquals(1, document.getValue(attribute.getKey()));
+				Assert.assertTrue(document.getAttributeValue(attribute.getKey()).getMetadata().getType()
+						.equals(RepositoryMetadataType.INTEGER));
+			}else if ("r_immutable_flag".equals(attribute.getKey())) {
+				Assert.assertEquals(true, document.getValue(attribute.getKey()));
+				Assert.assertTrue(document.getAttributeValue(attribute.getKey()).getMetadata().getType()
+						.equals(RepositoryMetadataType.BOOLEAN));
+			}else if ("r_full_content_size".equals(attribute.getKey())) {
+				Assert.assertEquals(new Double(1000456890), (Double)document.getValue(attribute.getKey()));
+				Assert.assertTrue(document.getAttributeValue(attribute.getKey()).getMetadata().getType()
+						.equals(RepositoryMetadataType.DOUBLE));
+			}else {
 				Assert.assertEquals(attribute.getValue(), document.getValue(attribute.getKey()).toString());
 			}
 		}
