fcbd0d34e71565fd1c9b34d542d579163ee4e853
eu2004 <utma.emilian@gmail.com>
2017-03-25 23:40:39 +0100

    updated

--- documentimporter/src/main/java/ro/eu/documentimporter/DocumentImporterAppConfig.java
+++
@@ -1,9 +0,0 @@
-package ro.eu.documentimporter;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"ro.eu.documentimporter"})
-public class DocumentImporterAppConfig {
-}
