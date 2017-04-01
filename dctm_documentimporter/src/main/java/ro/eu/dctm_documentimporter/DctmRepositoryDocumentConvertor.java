c8cde90ef70e20fd400e90ec8a3bf029523de164
eu2004 <utma.emilian@gmail.com>
2017-04-01 20:12:58 +0200

    updated

--- dctm_documentimporter/src/main/java/ro/eu/dctm_documentimporter/DctmRepositoryDocumentConvertor.java
+++ dctm_documentimporter/src/main/java/ro/eu/dctm_documentimporter/DctmRepositoryDocumentConvertor.java
@@ -35,7 +35,6 @@ public class DctmRepositoryDocumentConvertor implements RepositoryDocumentConver
 	@Override
 	public RepositoryDocument convert(Map<String, String> csvRecord) throws RepositoryException {
 		final DctmDocument document = new DctmDocument();
-		document.setFindCriteria(csvRecord.get("$DUPLICATE_SEARCH_CRITERIA"));
 		String type = csvRecord.get("r_object_type");
 		if (type == null) {
 			type = "dm_document";
@@ -56,12 +55,25 @@ public class DctmRepositoryDocumentConvertor implements RepositoryDocumentConver
 			attributeValue.setMetadata(attributeDefinition);
 			try {
 				attributeValue.setValue(getCellValue(attributeDefinition, cell.getValue()));
+				String findCriteria = processedFindCriteria(attributeValue, csvRecord.get("$DUPLICATE_SEARCH_CRITERIA"));
+				if (findCriteria.length() > 0) {
+					document.setFindCriteria(findCriteria);
+				}
 			} catch (ParseException e) {
 				throw new RepositoryException(e);
 			}
 			document.setAttributeValue(attributeValue);
 		}
+		
+		
 		return document;
+	}
+
+	private String processedFindCriteria(RepositoryEntityAttribute attributeValue, String findCriteria) {
+		if (findCriteria.indexOf("$" + attributeValue.getMetadata().getName()) != -1) {
+			return findCriteria.replaceAll("\\\\$" + attributeValue.getMetadata().getName(), attributeValue.getValue().toString());
+		}
+		return "";
 	}
 
 	private Object getCellValue(RepositoryMetadata attributeDefinition, String value) throws ParseException {
